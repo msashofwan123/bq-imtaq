9c719e572afe5918bafa7ad55d11682b81089716
msashofwan123 <msashofwan123@gmail.com>
2022-12-26 10:21:12 +0700

    upload file daftar dengan tambahan js

---
+++ daftarjs.js
@@ -0,0 +1,39 @@
+  $(document).ready(function() {
+    $('#form-submit').submit(function(event) {
+      event.preventDefault(); // mencegah form dari reload
+
+      // mengambil nilai dari form
+      var id = $('#id').val();
+      var nama = $('#nama').val();
+      var alamat = $('#alamat').val();
+      var tlahir = $('#tlahir').val();
+      var handphone = $('#handphone').val();
+      var alasan = $('#alasan').val();
+
+        // Mencegah Form Disubmit bila mana ada yang kosong  
+        if (nama == '' || alamat == '' || tlahir == '' || handphone == '' || alasan == '') {
+            // alert('Pastikan anda mengisi semua kolom!');
+            Swal.fire({
+                title: 'Peringatan',
+                text: 'pastikan anda mengisi semua kolom',
+                icon: 'warning', // tipe pesan (success, error, warning, info)
+            });
+            return;
+          }
+
+      // mengirim data ke server menggunakan AJAX
+      $.ajax({
+        type: 'POST',
+        url: 'daftarjs.php',
+        data: {id: id, nama: nama, alamat: alamat, tlahir: tlahir, handphone: handphone, alasan: alasan},
+        success: function(response) {
+          // menampilkan pesan sukses atau error
+          Swal.fire({
+            title: 'SELAMAT',
+            text: 'Data Pendaftaran anda sudah kami terima',
+            icon: 'success', // tipe pesan (success, error, warning, info)
+        });
+        }
+      });
+    });
+  });
---
+++ daftarjs.php
@@ -0,0 +1,23 @@
+<?php
+require_once('database/koneksi.php');
+
+// menerima data dari form
+$id = $_POST['id'];
+$nama = $_POST['nama'];
+$alamat = $_POST['alamat'];
+$tlahir = $_POST['tlahir'];
+$handphone = $_POST['handphone'];
+$alasan = $_POST['alasan'];
+
+// memasukkan data ke database
+$query = "INSERT INTO pendaftar (id, nama, alamat, tlahir, handphone, alasan) VALUES ('$id', '$nama', '$alamat', '$tlahir', '$handphone', '$alasan')";
+
+if (mysqli_query($conn, $query)) {
+    ?>
+    bodo amat
+    <?php
+} else {
+     echo "Error: " . $query . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
